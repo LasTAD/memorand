4dcba25e4437c0640d66a50e97b587eb93dc0404
Nikolai Kobyzev <kobyzev.n@gmail.com>
2020-05-23 17:02:11 +0300

    Minor changes

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from os.path import join, dirname
 
 setup(
     name='memorand',
-    version='1.5',
+    version='1.5-final',
     packages=find_packages(),
     long_description=open(join(dirname(__file__), 'README.md')).read(),
     install_requires=['requests', 'vk_api', 'psycopg2', 'PySimpleGUI', 'Pillow'],
